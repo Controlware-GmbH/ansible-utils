fe0480bf8d6279ae4d03afcf09439ed706271f0e
Korte Noack <korte.noack@controlware.de>
2023-06-27 13:25:13 +0200

    Filter 'cs_int_ranges' added

---
+++ controlware/utils/plugins/filter/integer_ranges.py
@@ -0,0 +1,166 @@
+"""Ansible and Jinja2 filter for handling comma-separated integer range lists"""
+
+
+def check_integer_value(value: str | int, max_int: int) -> int:
+    value = int(value)
+    if value < 0:
+        raise ValueError("Integer values must be >= 0")
+    if 0 < max_int < value:
+        raise ValueError(
+            "Integer value '%d' is higher than limit of %d" % (value, max_int)
+        )
+    return value
+
+
+def string_list_to_integer_list(string_list: str, max_int: int) -> list[int]:
+    """
+    Parse a string representing a comma-separated list of integer ranges
+    into a list of integers
+
+    Example:
+    str_list: ['100-103,105']
+    returns: [100, 101, 102, 103, 105]
+    """
+    integer_list = []
+    for element in string_list.split(","):
+        words = element.split("-")
+        if len(words) == 1:
+            integer_list.append(check_integer_value(element, max_int))
+        elif len(words) == 2:
+            start, end = words
+            integer_list.extend(
+                range(
+                    check_integer_value(start, max_int),
+                    check_integer_value(end, max_int) + 1,
+                )
+            )
+        else:
+            raise ValueError(
+                "'%s' is not a valid comma-separated integer range list." % string_list
+            )
+    return integer_list
+
+
+def cs_int_ranges(
+    values: list[str] | list[int] | str | dict,
+    max_int=0,
+    max_length=0,
+    max_length_next=None,
+):
+    """
+    cs_int_ranges will try to identify an integer information in values.
+    Each integer must be 0<=x<=max_int!
+    Then it builds a list of comma-separated integer range list strings.
+    Each string is not longer than max_length.
+
+    Parameters
+    ----------
+    values: None or str or list(str) or dict
+    max_int: int # >=0 ; 0 means don't use a limit
+    max_length: int # >= 11 # max length of string
+    max_length_next: int # >= 11 # max length of each string after first
+
+    Returns
+    -------
+    list(str)
+        list of comma-separated integer range strings
+
+    Example Jinja Code
+    ------------------
+    {% allowed_vlans | controlware.utils.cs_int_ranges() }}
+
+    Examples
+    --------
+    values: ['2-10', '13', '16', '200-299', '500-501']
+    max_length: 20
+    returns ['2-10,13,16,200-299', '500-501']
+    """
+
+    def add_range(str_list: list[str], s_id: int, e_id: int) -> list[str]:
+        if s_id < e_id:
+            new_range = "%d-%d" % (s_id, e_id)
+        else:
+            new_range = str(s_id)
+
+        # If list is empty, we just create first entry
+        if not str_list:
+            return [new_range]
+
+        __max = max_length
+        if max_length_next is not None and len(str_list) > 1:
+            __max = max_length_next
+
+        # We need to append new_range to last string
+        last_str = str_list[-1]
+        if 0 < __max < len(last_str) + len(",") + len(new_range):
+            str_list.append(new_range)
+        else:
+            last_str = last_str + "," + new_range
+            str_list = str_list[:-1] + [last_str]
+        return str_list
+
+    # Check 'max_int' parameter
+    if not isinstance(max_int, int) or max_int < 0:
+        raise ValueError("Parameter 'max_int' needs to be an integer and >= 0.")
+    # Check 'max_length' and 'max_length_next' parameters
+    if not isinstance(max_length, int) or (max_length != 0 and max_length < 11):
+        raise ValueError("Parameter 'max_length' needs to be an integer and >= 11.")
+    if max_length_next is not None and (
+        not isinstance(max_length_next, int)
+        or (max_length != 0 and max_length_next < 11)
+    ):
+        raise ValueError(
+            "Parameter 'max_length_next' needs to be an integer and >= 11."
+        )
+
+    # Check 'values'
+    if values is None:
+        return []
+    if isinstance(values, str):
+        if values == "all":
+            return ["all"]
+        values = string_list_to_integer_list(values, max_int)
+    elif isinstance(values, dict):
+        values = list(values.keys())
+    elif not isinstance(values, list):
+        raise ValueError("Given Integer information is not valid.")
+
+    # Convert strings to integers and sort list, if needed:
+    input_int_list = []
+    for i in values:
+        i_int = int(i)
+        if 0 < max_int < i_int or i_int < 0:
+            raise ValueError(
+                "Integer value '%d' is greather than limit of %d" % (i_int, max_int)
+            )
+        if i_int < 0:
+            raise ValueError("Unsupported negative integer value '%d'" % i_int)
+        input_int_list.append(i_int)
+    if not input_int_list:
+        return []
+    if len(input_int_list) == 1:
+        return [str(input_int_list[0])]
+
+    # We have more than 1 integer
+    input_int_list = sorted(input_int_list)
+    return_list = []
+    end_int = start_int = input_int_list[0]
+    for cur_int in input_int_list[1:]:
+        if cur_int == end_int + 1:
+            # This vlan ID extends current range
+            end_int = cur_int
+            continue
+        # We have a new range
+        return_list = add_range(return_list, start_int, end_int)
+        start_int = cur_int
+        end_int = cur_int
+    return_list = add_range(return_list, start_int, end_int)
+
+    return return_list
+
+
+class FilterModule:
+    def filters(self):
+        return {
+            "cs_int_ranges": cs_int_ranges,
+        }
---
+++ tests/unit/filter/test_cs_integer_ranges.py
@@ -0,0 +1,115 @@
+"""Pytest test definitions for cs_int_ranges filter"""
+import pytest
+from controlware.utils.plugins.filter.integer_ranges import FilterModule, cs_int_ranges
+
+
+TEST_PAIRS = [
+    (None, []),
+    ([], []),
+    ("1-3,5-6", ["1-3,5-6"]),
+    ("all", ["all"]),
+    ([2, 3, 4, 6, 7, 10, 11, 20], ["2-4,6-7,10-11,20"]),
+    ([2, 3, 4, 6, 10, 11, 20, 7], ["2-4,6-7,10-11,20"]),
+    (["1", "17", "18", "19", "2"], ["1-2,17-19"]),
+    ([1, 4094], ["1,4094"]),
+    ([1, 2, 4093, 4094], ["1-2,4093-4094"]),
+    ({}, []),
+    ({"1": {}, "2": {}, "5": {}, "6": {}}, ["1-2,5-6"]),
+    ({"1": {}, "5": {}, "6": {}, "2": {}}, ["1-2,5-6"]),
+]
+TEST_RAISE = [
+    [-1],
+    ["1-2-3"],
+    ["1-2a?i-3"],
+    ["1-2,a?i,-3"],
+]
+TEST_MAX_INT = [
+    ({"1": {}, "2": {}, "5": {}, "6": {}}, 15, ["1-2,5-6"]),
+]
+TEST_RAISE_MAX_INT = [
+    ({"1": {}, "2": {}, "5": {}, "16": {}}, 15),
+    ("1-2,4-16", 15),
+]
+TEST_MAX_LEN = [
+    (
+        [2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021],
+        16,
+        ["2001,2003,2005", "2007,2009,2011", "2013,2015,2017", "2019,2021"],
+    ),
+]
+TEST_RAISE_MAX_LEN = [
+    ([1, 2], 10),
+]
+TEST_MAX_LEN_NEXT = [
+    (
+        [2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021],
+        16,
+        11,
+        ["2001,2003,2005", "2007,2009", "2011,2013", "2015,2017", "2019,2021"],
+    ),
+]
+TEST_RAISE_MAX_LEN_NEXT = [
+    ([1, 2], 10),
+]
+
+f = FilterModule()
+
+
+class TestCiscoVlanListFilter:
+    @pytest.mark.parametrize("value, expected", TEST_PAIRS)
+    def test_correct_values(self, value, expected):
+        resp = cs_int_ranges(value)
+        assert resp == expected
+
+    @pytest.mark.parametrize("value", TEST_RAISE)
+    def test_raise_value_error(self, value):
+        try:
+            resp = cs_int_ranges(value)
+        except ValueError:
+            return
+        assert resp is None
+
+    @pytest.mark.parametrize("value, m_int, expected", TEST_MAX_INT)
+    def test_correct_values_with_max_int(self, value, m_int, expected):
+        resp = cs_int_ranges(value, m_int)
+        assert resp == expected
+
+    @pytest.mark.parametrize("value, m_int", TEST_RAISE_MAX_INT)
+    def test_raise_value_error_with_max_int(self, value, m_int):
+        try:
+            resp = cs_int_ranges(value, m_int)
+        except ValueError:
+            return
+        assert resp is None
+
+    @pytest.mark.parametrize("value, m_len, expected", TEST_MAX_LEN)
+    def test_correct_values_with_max_length(self, value, m_len, expected):
+        resp = cs_int_ranges(value, max_length=m_len)
+        assert resp == expected
+
+    @pytest.mark.parametrize("value, m_len", TEST_RAISE_MAX_LEN)
+    def test_raise_value_error_with_max_length(self, value, m_len):
+        try:
+            resp = cs_int_ranges(value, max_length=m_len)
+        except ValueError:
+            return
+        assert resp is None
+
+    @pytest.mark.parametrize("value, m_len, m_len_n, expected", TEST_MAX_LEN_NEXT)
+    def test_correct_values_with_max_length_next(self, value, m_len, m_len_n, expected):
+        resp = cs_int_ranges(value, max_length=m_len, max_length_next=m_len_n)
+        assert resp == expected
+
+    @pytest.mark.parametrize("value, m_len_n", TEST_RAISE_MAX_LEN_NEXT)
+    def test_raise_value_error_with_max_length_next(self, value, m_len_n):
+        try:
+            resp = cs_int_ranges(value, max_length=m_len_n)
+        except ValueError:
+            return
+        assert resp is None
+
+    def test_filter_registration(self):
+        resp = f.filters()
+        assert isinstance(resp, dict)
+        assert "cs_int_ranges" in resp
+        assert resp["cs_int_ranges"] == cs_int_ranges
