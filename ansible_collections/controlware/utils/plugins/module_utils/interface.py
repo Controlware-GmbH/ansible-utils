ab9f96655a187a55d3c9ffe24e8f018fd77e332c
Korte Noack <korte.noack@controlware.de>
2023-07-03 14:31:06 +0200

    Fix: __future__ imports

--- ansible_collections/controlware/utils/plugins/module_utils/interface.py
+++ ansible_collections/controlware/utils/plugins/module_utils/interface.py
@@ -1,5 +1,6 @@
 """Helper class to handle interface names"""
-from __future__ import absolute_import, annotations, division, print_function
+from __future__ import absolute_import, division, print_function
+from __future__ import annotations
 import re
 
 __metaclass__ = type
